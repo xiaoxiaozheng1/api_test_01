1b70b81a6c5b95c29243ddbfe3fb244cf52e2679
Administrator <907574415@qq.com>
2022-06-08 17:34:21 +0800

    更改run_case

--- run_case.py
+++ run_case.py
@@ -3,7 +3,7 @@ import unittest
 from HTMLTestRunner import HTMLTestRunner
 from api.base import Base
 
-test_report = 'test_report.html'
+test_report = 'test_reporter.html'
 
 if __name__ == '__main__':
     # 进行一次登录
@@ -15,4 +15,3 @@ if __name__ == '__main__':
     with open(test_report, 'wb') as f:
         runner = HTMLTestRunner(f, title='wshop测试报告', description='简化版的测试框架')
         runner.run(suite)
- 
